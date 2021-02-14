c181a810c1486da3029f353808caa3b46bfaf178
Satoshi-Kusumoto <29630164+Satoshi-Kusumoto@users.noreply.github.com>
2021-02-14 11:35:03 +0800

    inital compiles

--- pallets/swap/src/lib.rs
+++ pallets/swap/src/lib.rs
@@ -1,7 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
-use frame_system::ensure_signed;
+use frame_support::{
+    decl_event, decl_module, decl_storage, decl_error, ensure,
+    StorageMap, Parameter,
+};
+use sp_runtime::{
+    DispatchResult as Result, RuntimeDebug, ModuleId,
+    traits::{
+        Member, AtLeast32BitUnsigned, AccountIdConversion, Zero
+    }, 
+};
+use frame_system::{self as system, ensure_signed};
+use sp_std::prelude::*;
+use sp_std::{vec::Vec, convert::TryInto};
 
 #[cfg(test)]
 mod mock;
@@ -9,7 +20,13 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+const MAX_BOUND_TOKENS: usize = 8;
+
 pub trait Trait: frame_system::Trait {
+	type Balance: Member + Parameter + AtLeast32BitUnsigned + Default + Copy;
+
+	type AssetId: Parameter + AtLeast32BitUnsigned + Default + Copy;
+
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
@@ -29,8 +46,8 @@ decl_event!(
 
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		NoneValue,
-		StorageOverflow,
+		MaxTokens,
+		InvalidTokenLength,
 	}
 }
 
@@ -45,56 +62,69 @@ decl_module! {
 		#[weight = 10_000]
 		pub fn createPool(
 			origin,
-			tokens: Vec<AssetId>,
-			balance: Vec<Balance>,
+			tokens: Vec<T::AssetId>,
+			balance: Vec<T::Balance>,
 			denorm: Vec<u64>
-		) -> dispatch::DispatchResult {
-		
+		) -> Result {
+
+			// check inputs
+			ensure!(tokens.len() < MAX_BOUND_TOKENS, Error::<T>::MaxTokens);
+			ensure!(balance.len() == tokens.len(), Error::<T>::InvalidTokenLength);
+			ensure!(denorm.len() == tokens.len(), Error::<T>::InvalidTokenLength);
+
+			// TODO: check duplication
+			// TODO: sort tokens to get a unique identifier of the pool and check if exists
+	
+			Ok(())
 		}
 		
 		#[weight = 10_000]
 		pub fn joinPool(
 			origin,
 			poolId: u64,
-			poolAmountOut: Balance,
-			maxAmountsIn: Vec<Balance>
-		) -> dispatch::DispatchResult {
+			poolAmountOut: T::Balance,
+			maxAmountsIn: Vec<T::Balance>
+		) -> Result {
 		
+			Ok(())
 		}
 
 		#[weight = 10_000]
 		pub fn exitPool(
 			origin,
 			poolId: u64,
-			poolAmountIn: Balance,
-			minAmountsOut: Vec<Balance>
-		) -> dispatch::DispatchResult {
+			poolAmountIn: T::Balance,
+			minAmountsOut: Vec<T::Balance>
+		) -> Result {
 		
+			Ok(())
 		}
 
 		#[weight = 10_000]
 		pub fn swapExactAmountIn(
 			origin,
 			poolId: u64,
-			tokenIn: AssetId,
-			tokenAmountIn: Balance,
-			tokenOut: AssetId,
-			minAmountOut: Balance,
-			maxPrice: Balance
-		) -> dispatch::DispatchResult {
+			tokenIn: T::AssetId,
+			tokenAmountIn: T::Balance,
+			tokenOut: T::AssetId,
+			minAmountOut: T::Balance,
+			maxPrice: T::Balance
+		) -> Result {
 
+			Ok(())
 		}
 
 		#[weight = 10_000]
 		pub fn swapExactAmountOut(
 			origin, 
 			poolId: u64,
-			tokenIn: AssetId,
-			maxAmountIn: Balance,
-			tokenOut: AssetId,
-			tokenAmountOut: Balance,
-			maxPrice: Balance) -> dispatch::DispatchResult {
-
+			tokenIn: T::AssetId,
+			maxAmountIn: T::Balance,
+			tokenOut: T::AssetId,
+			tokenAmountOut: T::Balance,
+			maxPrice: T::Balance) -> Result {
+			
+			Ok(())
 		}
 
 	}
